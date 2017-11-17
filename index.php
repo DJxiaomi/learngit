64480fa014a11aaf94a0cc0c3be6782d87c38472
Xiaomi <xiaomi.ye@qq.com>
2017-11-17 15:18:23 +0800

    提交index

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>第三课教育培训信息平台</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link type="image/x-icon" href="/favicon.ico" rel="icon">
+	<link href="/resource/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="/views/mobile/javascript/mui/css/mui.min.css" rel="stylesheet" type="text/css" />
+	<link href="/views/mobile/skin/blue/css/common.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="/resource/scripts/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="/views/mobile/javascript/mui/js/mui.min.js"></script>
+	<script type="text/javascript" src="/resource/scripts/common.js"></script>
+	<script type="text/javascript">mui.init({swipeBack:true});var SITE_URL = '/';</script>
+
+<meta name='keywords' content='教育培训,第三课教育培训信息平台,选课程,找老师,挑学校,招生宣传,在线报名,教育培训一站式服务,学费优惠,代金券,抵学费,优惠叠加,代金券抵扣学费,体验课,短期,4节课,6节课,8节课,短期体验课,4节至8节短期体验课程,免费,试听课,免费试听课,报告,测评,现场体验,真实感受,发布课程评测报告,信息,活动,互动,学员交流,教育资讯,学前教育,中小学课程,文学艺术,职业技能,益智体育,教育培训信息展示'>
+<meta name='description' content='提供免费试听课、短期体验课、学费抵扣券等多项优惠,对课程提前试听,发布客观全面的体验报告,让您对教育培训机构进行提前了解,减少教育支出，让教育更轻松'></head>
+<body>
+		<header class="mui-bar mui-bar-nav">
+				    	<a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+				    	    <h1 class="mui-title">订单结算</h1>
+	</header>
+		<div class="mui-content">
+	<script type="text/javascript" charset="UTF-8" src="/runtime/_systemjs/my97date/wdatepicker.js"></script>
+<script type="text/javascript" charset="UTF-8" src="/runtime/_systemjs/artTemplate/artTemplate.js"></script><script type="text/javascript" charset="UTF-8" src="/runtime/_systemjs/artTemplate/artTemplate-plugin.js"></script>
+<link href="/views/mobile/skin/blue/css/cart.css" rel="stylesheet" type="text/css" />
+<style>
+.list_table input[type=text], .list_table select {width: auto;}
+.notice {margin-left:0px;margin-top:15px;}
+.notice b {color:red;font-size:12px;}
+.teaching_time2 {padding-left:3%;font-size:100%;}
+.teaching_time2 select, .teaching_time2 input {width:auto;}
+.teaching_time2 input {border:1px solid #666;height:22px;line-height:22px; width: 80px;}
+.list_table input {height: 25px; padding: 0px; border: 1px solid #ccc;}
+.list_table tr {height: 28px; line-height: 28px;}
+.form_table2 { border-bottom: 1px solid #f4f4f4; margin-bottom: 8px;}
+.form_table2 th {width: 79px;}
+.form_table2 td input {width: 60px; font-size: 100%; text-indent: 3px;}
+</style>
+<script type='text/javascript' src='/views/mobile/javascript/orderFormClass.js'></script>
+<form action='/simple/cart3n' method='post' name='order_form' id="order_form">
+	<input type='hidden' name='timeKey' value='1510819376' />
+	<input type='hidden' name='direct_gid' value='3995' />
+	<input type='hidden' name='direct_type' value='product' />
+	<input type='hidden' name='direct_num' id="direct_num" value='1' />
+	<input type='hidden' name='direct_promo' value='' />
+	<input type='hidden' name='direct_active_id' value='0' />
+	<input type='hidden' name='takeself' value='0' />
+	<h5 class="mui-content-padded">联系人：</h5>
+	<div class="mui-card">
+		<div class="mui-input-group" id="address_form">
+						<div class="mui-input-row mui-radio mui-left">
+				<label>龚玲&nbsp;&nbsp;18975317018</label>
+				<input name="radio_address" type="radio" value="856" onclick='orderFormInstance.addressSelected({"id":"856","user_id":"31072","accept_name":"龚玲","zip":"000000","telphone":"","country":null,"province":"111111","city":"111111","area":"111111","address":"默认地址","mobile":"18975317018","is_default":"1","province_val":"","city_val":"","area_val":""});' data-json='{"id":"856","user_id":"31072","accept_name":"龚玲","zip":"000000","telphone":"","country":null,"province":"111111","city":"111111","area":"111111","address":"默认地址","mobile":"18975317018","is_default":"1","province_val":"","city_val":"","area_val":""}'>
+			</div>
+	    	    <div class="mui-input-row mui-radio mui-left">
+				<label>新联系人</label>
+				<input name="radio_address" type="radio" value="" onclick='orderFormInstance.addressEmpty();'>
+			</div>
+						<div class="addr_list" id="acceptbox" style="display:none;">
+				<div class="mui-input-row">
+					<!-- <label>联系人姓名</label> -->
+					<input type="text" class="mui-input-clear mui-input" id="accept_name" name="accept_name" value="" placeholder="姓名" />
+				</div>
+				<div class="mui-input-row">
+					<!-- <label>手机号码</label> -->
+					<input type="text" class="mui-input-clear mui-input" name="mobile" id="mobile" value="" placeholder="手机号码" />
+				</div>
+				<input type="hidden" name="province" value="111111">
+        <input type="hidden" name="city" value="111111">
+        <input type="hidden" name="area" value="111111">
+								<input type="hidden" name="address" value="收货地址">
+				        <input type="hidden" name="phone" value="123456">
+        <input type="hidden" name="zip" value="000000">
+				<input type="hidden" name="category_id" value="0" />
+				<input type="hidden" name="manual_id" value="0" />
+			</div>
+		</div>
+	</div>
+
+	<h5 class="mui-content-padded">支付方式</h5>
+	<div class="mui-card">
+		<div class="mui-input-group">
+				    		    														<div class="mui-input-row mui-radio mui-left">
+					<label>第三课余额支付</label>
+					<input name="payment" type="radio" alt="0" value="1" onclick='orderFormInstance.paymentSelected({"id":"1","name":"第三课余额支付","type":"1","class_name":"balance","logo":"\/payments\/logos\/pay_deposit.gif","status":"0","order":"99","note":"","poundage":"0.00","poundage_type":"1","client_type":"3"});' title="第三课余额支付">
+				</div>
+										<div class="mui-input-row mui-radio mui-left">
+					<label>微信支付</label>
+					<input name="payment" type="radio" value="14" checked>
+				</div>
+				  			</div>
+	</div>
+
+	
+	<h5 class="mui-content-padded">配送方式</h5>
+	<div class="mui-card" style="display:none;">
+		<div class="mui-input-group">
+							<div class="mui-input-row mui-radio mui-left">
+					<label>现场教学</label>
+					<input name="delivery_id" type="radio" paytype="0" value="1" onclick='orderFormInstance.deliverySelected(1);' title="现场教学">
+				</div>
+							<div class="mui-input-row mui-radio mui-left">
+					<label>网络教学</label>
+					<input name="delivery_id" type="radio" paytype="0" value="3" onclick='orderFormInstance.deliverySelected(3);' title="网络教学" checked>
+				</div>
+					</div>
+	</div>
+
+	<h5 class="mui-content-padded"></h5>
+	<div class="mui-card">
+		<div class="mui-input-group">
+			<div class="mui-input-row">
+				<label>留言</label>
+				<input type="text" name="message" value="" placeholder="输入留言" />
+			</div>
+		</div>
+	</div>
+
+	<h5 class="mui-content-padded"></h5>
+	<div>
+		<ul class="mui-card">
+					    <li class="mui-table-view-cell mui-media">
+		    	<img class="mui-media-object mui-pull-left" src="/pic/thumb/img/deX2B9sab32eFdk5L3z5I9weM3T7cev6MaD4M6v8M0z0E5v8MejeA1xfN4zeA2zfMzExNDM1NTYxMTYuanBnL3cvODAvaC84MAO0O0OO0O0O">
+	    		<div class="mui-media-body">
+	    			小小建筑师主题课					<div class="qindanjg">价格：<span class="red3">&yen;1000.00</span></div>
+					<div>人数：<span class="red3">1&nbsp;人</span></div>
+	    		</div>
+		    </li>
+		    		</ul>
+	</div>
+
+	<div class="mui-content-padded">
+		<button type="button" class="mui-btn mui-btn-primary mui-btn-block" id="confirm">确认无误，提交订单</button>
+	</div>
+</form>
+
+<script type='text/javascript'>
+//创建订单表单实例
+orderFormInstance = new orderFormClass();
+sellerList = {"376":1000};
+ticketList = null;addressList = [{"id":"856","user_id":"31072","accept_name":"\u9f9a\u73b2","zip":"000000","telphone":"","country":null,"province":"111111","city":"111111","area":"111111","address":"\u9ed8\u8ba4\u5730\u5740","mobile":"18975317018","is_default":"1","province_val":"","city_val":"","area_val":""}];
+addressList = [{"id":"856","user_id":"31072","accept_name":"\u9f9a\u73b2","zip":"000000","telphone":"","country":null,"province":"111111","city":"111111","area":"111111","address":"\u9ed8\u8ba4\u5730\u5740","mobile":"18975317018","is_default":"1","province_val":"","city_val":"","area_val":""}];
+sell_count = 0.00;
+market_count = 0.00;
+
+var _max_cprice = 0;
+var _max_order_chit = 0;
+var _cprice = 0;
+var _order_chit = 0;
+var _order_amount = 1000;
+
+//DOM加载完毕
+jQuery(function(){
+
+	orderFormInstance.addressInit("856", "", "");
+
+	//支付方式
+	orderFormInstance.paymentInit("14");
+
+	// 支付方式自动保存
+	$('input[name=payment]').click(function(){
+		orderFormInstance.paymentSave();
+	})
+
+	$('.add_teaching_time').click(function(){
+		$('.teaching_time2 ul').append('<li>' + $('.teaching_time2 li').eq(0).html() + '</li>');
+	})
+
+	// 地址自动保存
+	$('.addr_list input[name=radio_address]').click(function(){
+		var _radio_id = $(this).val();
+		if ( _radio_id != '' )
+		{
+			address_save();
+		}
+	})
+
+	function caculate()
+	{
+		var _coupon_nums = $("input[name=coupon_nums]").val();
+		if ( $('input[name=use_coupon]').val() == '1' )
+		{
+			var _sum = market_count - _coupon_nums;
+			var _t_yh = _order_chit - parseFloat($("input[name=coupon_nums]").val());
+			$('.yh').html( _t_yh );
+		} else {
+			var _sum = market_count;
+		}
+
+		$('.yf_count').html(_sum);
+	}
+
+	document.getElementById('confirm').addEventListener('tap', function(){
+		submitForm();
+	});
+
+	document.getElementById('add').addEventListener('tap', function(){
+		var num = document.getElementById('num').value;
+		document.getElementById('direct_num').value = num;
+	});
+
+	document.getElementById('minus').addEventListener('tap', function(){
+		var num = document.getElementById('num').value;
+		document.getElementById('direct_num').value = num;
+	});
+});
+
+//[address]保存到常用的地址
+function address_save()
+{
+	if(orderFormInstance.addressCheck())
+	{
+		if ( addressList.length > 0 )
+		{
+			var _accept_name = $('input[name=accept_name]').val();
+			var _mobile = $('input[name=mobile]').val();
+			if ( _accept_name == '' || _mobile == '')
+			{
+				mui.toast('姓名或联系方式不能为空');
+				return false;
+			}
+			for( var i = 0; i < addressList.length; i++)
+			{
+				if ( addressList[i]['accept_name'] == _accept_name && addressList[i]['mobile'] == _mobile )
+				{
+					$('.addr_list').find("input[type=radio]").eq(i).attr("checked","checked");
+					orderFormInstance.addressSave();
+					$('#order_form').submit();
+					return;
+				}
+			}
+		}
+		$.getJSON('/simple/address_add',$('form[name="order_form"]').serialize(),function(content){
+			if(content.data)
+			{
+				$('input:radio[name="radio_address"]:first').trigger('click');
+				$('input:radio[name="radio_address"]:first').val(content.data.id);
+				$('input[name=accept_name]').val(content.data.accept_name);
+				$('input[name=mobile]').val(content.data.mobile);
+				$('#order_form').submit();
+				return;
+			}
+			//orderFormInstance.addressSave();
+		});
+		var _address_info = [];
+		var _accept_name = $('input[name=accept_name]').val();
+		var _mobile = $('input[name=mobile]').val();
+		_address_info['accept_name'] = _accept_name;
+		_address_info['mobile'] = _mobile;
+		addressList.push(_address_info);
+	}
+}
+
+function submitForm(){
+	var accept_name = document.getElementById('accept_name').value,
+		mobile = document.getElementById('mobile').value;
+	var _address = $('input[name=address]').val();
+	if(accept_name == ''){
+		mui.alert('请填写联系人', '提示信息');
+		return false;
+	}
+	if(mobile == ''){
+		mui.alert('请填写手机号码', '提示信息');
+		return false;
+	}
+		if($('input:radio[name="payment"]:checked').length <= 0 )
+	{
+		mui.alert('请选择支付方式', '提示信息');
+		return false;
+	}
+	if ( !orderFormInstance.deliveryCheck())
+	{
+		mui.alert('请选择配送方式', '提示信息');
+		return false;
+	}
+
+	address_save();
+	//$('#order_form').submit();
+}
+</script>
+
+	</div>
+	<nav class="mui-bar mui-bar-tab">
+	    <a class="mui-tab-item home " href="/">
+	        <span class="mui-tab-label">首页</span>
+	    </a>
+			<a class="mui-tab-item gift " href="/site/chit1" id="ztelBtn">
+	        <span class="mui-tab-label">短期课</span>
+	    </a>
+			<a class="mui-tab-item brand " href="/site/manual" id="ztelBtn">
+					<span class="mui-tab-label">学习通</span>
+			</a>
+	    <a class="mui-tab-item user " href="/simple/login?callback=/ucenter" id="ltelBtn">
+	        <span class="mui-tab-label">我的</span>
+	    </a>
+      <a class="mui-tab-item service" href="/site/service" id="ltelBtn">
+	        <span class="mui-tab-label">客服</span>
+	    </a>
+	</nav>
+</body>
+</html>
+<script type="text/javascript" src="/views/mobile/javascript/mui/js/mui.lazyload.js"></script>
+<script type="text/javascript" src="/views/mobile/javascript/mui/js/mui.lazyload.img.js"></script>
+<script type="text/javascript">
+mui('body').on('tap', 'a', function(){
+    if(this.href != '#' && this.href != ''){
+        mui.openWindow({
+            url: this.href
+        });
+    }
+    this.click();
+});
+
+</script>
+
+<script type="text/javascript">
+_webUrl = "/_controller_/_action_/_paramKey_/_paramVal_";_themePath = "/views/mobile/";_skinPath = "/views/mobile/skin/blue/";
+//创建URL地址
+function creatUrl(param)
+{
+	var urlArray   = [];
+	var _tempArray = param.split("/");
+	for(var index in _tempArray)
+	{
+		if(_tempArray[index])
+		{
+			urlArray.push(_tempArray[index]);
+		}
+	}
+
+	if(urlArray.length >= 2)
+	{
+		var iwebshopUrl = _webUrl.replace("_controller_",urlArray[0]).replace("_action_",urlArray[1]);
+
+		//存在URL参数情况
+		if(urlArray.length >= 4)
+		{
+			iwebshopUrl = iwebshopUrl.replace("_paramKey_",urlArray[2]);
+
+			//卸载原数组中已经拼接的数据
+			urlArray.splice(0,3);
+
+			if(iwebshopUrl.indexOf("?") == -1)
+			{
+				iwebshopUrl = iwebshopUrl.replace("_paramVal_",urlArray.join("/"));
+			}
+			else
+			{
+				var _paramVal_ = "";
+				for(var i in urlArray)
+				{
+					if(i == 0)
+					{
+						_paramVal_ += urlArray[i];
+					}
+					else if(i%2 == 0)
+					{
+						_paramVal_ += "="+urlArray[i];
+					}
+					else
+					{
+						_paramVal_ += "&"+urlArray[i];
+					}
+				}
+				iwebshopUrl = iwebshopUrl.replace("_paramVal_",_paramVal_);
+			}
+		}
+		return iwebshopUrl;
+	}
+	return '';
+}
+
+//切换验证码
+function changeCaptcha()
+{
+	$('#captchaImg').prop('src',creatUrl("site/getCaptcha/random/"+Math.random()));
+}
+</script>
